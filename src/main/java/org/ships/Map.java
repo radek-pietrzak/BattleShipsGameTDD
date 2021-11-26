b69101fef81b4b770a0920d07586a85b92294292
radek <radekpietrzak102@gmail.com>
2021-11-26 09:14:25 +0100

    draw two maps

--- src/main/java/org/ships/Map.java
+++ src/main/java/org/ships/Map.java
@@ -2,20 +2,36 @@ package org.ships;
 
 public class Map {
 
-    public String drawMap(Matrix matrix) {
+    private Matrix matrixAlly = new Matrix();
+    private Matrix matrixEnemy = new Matrix();
+
+    public String drawMaps(Matrix matrixAlly, Matrix matrixEnemy) {
 
         StringBuilder sb = new StringBuilder();
 
-        sb.append("   A|B|C|D|E|F|G|H|I|J|\r\n");
+        sb.append("   A|B|C|D|E|F|G|H|I|J|");
+        sb.append("     A|B|C|D|E|F|G|H|I|J|\r\n");
 
         for (int i = 0; i < 10; i++) {
             sb.append(i + 1);
             if (i < 9)
                 sb.append(" ");
+
             for (int j = 0; j < 10; j++) {
                 sb.append("|");
-                sb.append(matrix.getMatrix()[j][i]);
+                sb.append(matrixAlly.getMatrix()[j][i]);
             }
+
+            sb.append("|  ");
+            sb.append(i + 1);
+            if (i < 9)
+                sb.append(" ");
+
+            for (int k = 0; k < 10; k++) {
+                sb.append("|");
+                sb.append(matrixEnemy.getMatrix()[k][i]);
+            }
+
             sb.append("|\r\n");
         }
 
--- src/test/java/org/ships/MapTest.java
+++ src/test/java/org/ships/MapTest.java
@@ -10,25 +10,25 @@ class MapTest {
     Matrix matrix = new Matrix();
 
     @Test
-    void shouldDrawEmptyMap() {
+    void shouldDrawEmptyMaps() {
 
         //given
         String result = """
-                   A|B|C|D|E|F|G|H|I|J|\r
-                1 | | | | | | | | | | |\r
-                2 | | | | | | | | | | |\r
-                3 | | | | | | | | | | |\r
-                4 | | | | | | | | | | |\r
-                5 | | | | | | | | | | |\r
-                6 | | | | | | | | | | |\r
-                7 | | | | | | | | | | |\r
-                8 | | | | | | | | | | |\r
-                9 | | | | | | | | | | |\r
-                10| | | | | | | | | | |\r
+                   A|B|C|D|E|F|G|H|I|J|     A|B|C|D|E|F|G|H|I|J|\r
+                1 | | | | | | | | | | |  1 | | | | | | | | | | |\r
+                2 | | | | | | | | | | |  2 | | | | | | | | | | |\r
+                3 | | | | | | | | | | |  3 | | | | | | | | | | |\r
+                4 | | | | | | | | | | |  4 | | | | | | | | | | |\r
+                5 | | | | | | | | | | |  5 | | | | | | | | | | |\r
+                6 | | | | | | | | | | |  6 | | | | | | | | | | |\r
+                7 | | | | | | | | | | |  7 | | | | | | | | | | |\r
+                8 | | | | | | | | | | |  8 | | | | | | | | | | |\r
+                9 | | | | | | | | | | |  9 | | | | | | | | | | |\r
+                10| | | | | | | | | | |  10| | | | | | | | | | |\r
                 """;
         //when
         //then
-        assertEquals(result, map.drawMap(matrix));
+        assertEquals(result, map.drawMaps(matrix, matrix));
     }
 
 }
